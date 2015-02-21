d74920c9972202b71cec226c5336779a18d33e03
Michal Hoftich <michal.h21@gmail.com>
2015-02-21 19:54:09 +0100

    Detecting marks

--- normalize.lua
+++ normalize.lua
@@ -8,7 +8,7 @@ local name = unicode.conformance.name
 local byte = unicode.utf8.byte
 local unidata = characters.data
 
-local function make_hash = function(t) 
+local function make_hash (t) 
   local y = {}
   for _,v in ipairs(t) do 
     y[v] = true
@@ -16,7 +16,10 @@ local function make_hash = function(t)
   return y
 end
 
-local letter_categories = 
+local letter_categories = make_hash {"lu","ll","lt","lo","lm"}
+
+local mark_categories = make_hash {"mn","mc","me"}
+
 local function printchars(s)
 	local t = {}
 	for x in gmatch(s,".") do
@@ -25,15 +28,57 @@ local function printchars(s)
 	print(table.concat(t,":"))
 end
 
+local categories = {}
+
+
+local function get_category(charcode)
+  local category = categories[charcode] or unidata[charcode].category
+  categories[charcode] = category
+  return category
+end
+
+-- get glyph char and category
+local function glyph_info(n)
+  local char = n.char
+  return char, get_category(char)
+end
+
+local function normalize_marks(head, n)
+  print "máme mark"
+  return head, n.next
+end
+
+local function normalize_glyphs(head, n)
+  --local charcode = n.char
+  --local category = get_category(charcode)
+  local charcode, category = glyph_info(n)
+  if letter_categories[category] then 
+    local nextn = n.next
+    if nextn.id == 37 then
+      local nextchar = nextn.char
+      local nextcat = get_category(nextchar)
+      if mark_categories[nextcat] then
+        return normalize_marks(head,n)
+      end
+    end
+  end
+  return head, n.next 
+end
+
+
 function M.nodes(head)
 	local t = {}
 	local text = false
-	for n in node.traverse(head) do
+  local n = head
+	-- for n in node.traverse(head) do
+  while n do
 		if n.id == 37 then
-			print(name(n.char))
-			print(unidata[n.char].category)
-			t[#t+1]= char(n.char)
+      local charcode = n.char
+			print(name(charcode))
+			print(get_category(charcode))
+			t[#t+1]= char(charcode)
 			text = true
+      head, n = normalize_glyphs(head, n)
 		else
 			if text then
 				local s = table.concat(t)
@@ -43,6 +88,7 @@ function M.nodes(head)
 			end
 			text = false
 			t = {}
+      n = n.next
 		end
 	end
 	return head
