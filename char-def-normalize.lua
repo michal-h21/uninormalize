934528bb3f89e7e1e90606fc49eae3ebcdc2735a
Michal Hoftich <michal.h21@gmail.com>
2015-05-21 16:05:12 +0200

    Use char-def-witch-ccc

--- char-def-normalize.lua
+++ char-def-normalize.lua
@@ -1,10 +1,11 @@
 local unicharacters = {}
 
 
-if not characters then dofile('char-def.lua') end
+-- if not characters then dofile('char-def.lua') end
 --if not characters then dofile('char-def-with-ccc.lua') end
--- local characters = require "char-def-with-ccc.lua"
+local characters = require "char-def-with-ccc.lua"
 
 unicharacters.data = characters.data
+
 
 return unicharacters
